c446f0073c4f51720f56d38c931402d21094cc79
WebmasterShane <shanelwebber@gmail.com>
2020-11-07 10:43:48 -0800

    Index2 update

--- script.js
+++ script.js
@@ -12,6 +12,84 @@ $(document).ready(function () {
       $("#img1").append("<img src=" + currentMeme + ">");
     });
   }
+  function displayMeme2() {
+    var queryURL = "https://meme-api.herokuapp.com/gimme";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var currentMeme = response.url;
+      console.log(response);
+
+      $("#img2").append("<img src=" + currentMeme + ">");
+    });
+  }
+  function displayMeme3() {
+    var queryURL = "https://meme-api.herokuapp.com/gimme";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var currentMeme = response.url;
+      console.log(response);
+
+      $("#img3").append("<img src=" + currentMeme + ">");
+    });
+  }
+  function displayMeme4() {
+    var queryURL = "https://meme-api.herokuapp.com/gimme";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var currentMeme = response.url;
+      console.log(response);
+
+      $("#img4").append("<img src=" + currentMeme + ">");
+    });
+  }
+  function displayMeme5() {
+    var queryURL = "https://meme-api.herokuapp.com/gimme";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var currentMeme = response.url;
+      console.log(response);
+
+      $("#img5").append("<img src=" + currentMeme + ">");
+    });
+  }
+  function displayMeme6() {
+    var queryURL = "https://meme-api.herokuapp.com/gimme";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var currentMeme = response.url;
+      console.log(response);
+
+      $("#img6").append("<img src=" + currentMeme + ">");
+    });
+  }
+  function displayMeme7() {
+    var queryURL = "https://meme-api.herokuapp.com/gimme";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var currentMeme = response.url;
+      console.log(response);
+
+      $("#img7").append("<img src=" + currentMeme + ">");
+    });
+  }
   function displayJoke() {
     var jokeURL = "https://official-joke-api.appspot.com/random_joke";
 
@@ -38,5 +116,11 @@ $(document).ready(function () {
 
 
   displayMeme1();
+  displayMeme2();
+  displayMeme3();
+  displayMeme4();
+  displayMeme5();
+  displayMeme6();
+  displayMeme7();
   displayJoke();
 });

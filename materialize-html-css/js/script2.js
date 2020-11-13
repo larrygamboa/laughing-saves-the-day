e14210ec532e5df0974dad3d7da7d39eb7205e8f
WebmasterShane <shanelwebber@gmail.com>
2020-11-12 22:04:58 -0800

    updating file paths

--- materialize-html-css/js/script2.js
+++ materialize-html-css/js/script2.js
@@ -1,20 +1,41 @@
 $(document).ready(function () {
   function displayMeme() {
-    var queryURL = "https://meme-api.herokuapp.com/gimme/dankmemes/7";
-    console.log(queryURL)
+    var getMemeInput = localStorage.getItem("memeInput");
+    console.log(getMemeInput);
 
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      for(i=0; i<7; i++){
-      var currentMeme = response.memes[i].url
-      console.log(currentMeme);
+    if (getMemeInput === null) {
+      var queryURL = "https://meme-api.herokuapp.com/gimme/dankmemes/12";
 
-      $("#img"+i).append("<img src=" + currentMeme + ">");}
-    });
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        for (i = 0; i < 12; i++) {
+          var currentMeme = response.memes[i].url;
+          console.log(currentMeme);
+
+          $("#img" + i).html("<img src=" + currentMeme + ">");
+        }
+      });
+    } else {
+      var queryURL =
+        "https://meme-api.herokuapp.com/gimme/" + getMemeInput + "/12";
+      console.log(queryURL);
+
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        for (i = 0; i < 12; i++) {
+          var currentMeme = response.memes[i].url;
+          console.log(currentMeme);
+
+          $("#img" + i).html("<img src=" + currentMeme + ">");
+        }
+      });
+    }
   }
-  
+
   function displayJoke() {
     var jokeURL = "https://official-joke-api.appspot.com/random_joke";
 
@@ -29,25 +50,67 @@ $(document).ready(function () {
 
       console.log(response.punchline);
 
-        $("#jokeButton").on("click", function (event) {
-    var joke = "whatever";
-    console.log(joke);
-    event.preventDefault();
-    $("#setup").html(response.punchline);
-    console.log(response.punchline);
-  });
+      $("#jokeButton").on("click", function (event) {
+        var joke = "whatever";
+        console.log(joke);
+        event.preventDefault();
+        $("#setup").html(response.punchline);
+        $("#nextJoke").html(
+          '<button id="nextBtn" class="btn-large waves-effect waves-light teal lighten-1">Next Joke</button>'
+        );
+        $("#nextBtn").on("click", function (event) {
+          displayMeme();
+          $("#setup").html("");
+          $("#nextJoke").html(
+            '<button id="jokeButton" class="btn-large waves-effect waves-light teal lighten-1">Punchline</button>'
+          );
+          displayJoke();
+        });
+      });
     });
   }
 
-
   displayMeme();
   displayJoke();
+  $("#meme-Search").on("click", function (event) {
+    event.preventDefault();
+    var memeinuput = $("#meme-input").val();
+    memeinuput = memeinuput.replace(/\s/g, "");
+    localStorage.setItem("memeInput", memeinuput);
+    console.log($("#meme-input").val());
+    function displayMeme2() {
+      var queryURL = `https://meme-api.herokuapp.com/gimme/${memeinuput}/12`;
+      console.log(queryURL);
+      console.log(memeinuput);
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        for (i = 0; i < 12; i++) {
+          var currentMeme = response.memes[i].url;
+          console.log(currentMeme);
+
+          $("#img" + i).html("<img src=" + currentMeme + ">");
+        }
+      });
+    }
+    displayMeme2();
+  });
+
+  $("#impactpg").on("click", function () {
+    window.location.href = "index2.html";
+    console.log("test Test");
+  });
 });
 
-$("#impactpg").on("click", function (){
-  
-  window.location.href="index2.html"
-  console.log("test Test")
-
-})
-
+$(document).ready(function () {
+  $(window).resize(function () {
+    if ($(window).width() < 769) {
+      $("#mediaAdjust").removeClass("row");
+    }
+    else {
+      $("#mediaAdjust").addClass("row");
+    }
+  });
+  resize();
+});
